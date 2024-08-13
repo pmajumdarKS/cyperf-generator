0adb3ece77ea50c13cade4f72e55a6c181a6b7ce
Partha Majumdar <partha.majumdar@keysight.com>
2024-08-13 00:02:05 +0000

    Renamed python package to `cyperf`.

--- generate.sh
+++ generate.sh
@@ -3,5 +3,4 @@
 mkdir -p logs
 
 cat specs/cyperf-openapi.json | python -c 'import sys, yaml, json; print(yaml.dump(json.loads(sys.stdin.read())))' | tee cyperf/cyperf-openapi.yaml
-docker run --rm -v ${PWD}:/local --user=$(id -u):$(id -g) openapitools/openapi-generator-cli generate -i /local/cyperf/cyperf-openapi.yaml -g python -o /local/cyperf 2>&1 | tee logs/generate.log
-mv cyperf/openapi_client cyperf/rest
+docker run --rm -v ${PWD}:/local --user=$(id -u):$(id -g) openapitools/openapi-generator-cli generate --package-name cyperf -i /local/cyperf/cyperf-openapi.yaml -g python -o /local/cyperf 2>&1 | tee logs/generate.log
