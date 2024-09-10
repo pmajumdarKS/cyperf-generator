bed748a9d73fc71a04d6694aef9d186c96af1a93
Partha Majumdar <partha.majumdar@keysight.com>
2024-09-10 06:20:56 +0000

    Updated spec for dynamic python wrapper creation.

--- generate.sh
+++ generate.sh
@@ -3,4 +3,4 @@
 mkdir -p logs
 
 cat specs/cyperf-openapi.json | python -c 'import sys, yaml, json; print(yaml.dump(json.loads(sys.stdin.read())))' | tee cyperf/cyperf-openapi.yaml
-docker run --rm -v ${PWD}:/local --user=$(id -u):$(id -g) openapitools/openapi-generator-cli generate --package-name cyperf -i /local/cyperf/cyperf-openapi.yaml -g python -o /local/cyperf 2>&1 | tee logs/generate.log
+docker run --rm -v ${PWD}:/local --user=$(id -u):$(id -g) openapitools/openapi-generator-cli generate --package-name cyperf -i /local/cyperf/cyperf-openapi.yaml -c /local/specs/config.yaml -g python -o /local/cyperf 2>&1 | tee logs/generate.log
